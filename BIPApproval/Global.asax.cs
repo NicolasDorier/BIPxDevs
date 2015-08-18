1165ba097d4a2215d246d21a1f049a5b379eb2e0
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-08-18 19:38:01 +0900

    update packages

--- BIPApproval/Global.asax.cs
+++ BIPApproval/Global.asax.cs
@@ -18,7 +18,7 @@ namespace BIPApproval
 		{
 			AreaRegistration.RegisterAllAreas();
 
-			WebApiConfig.Register(GlobalConfiguration.Configuration);
+			//WebApiConfig.Register(GlobalConfiguration.Configuration);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
