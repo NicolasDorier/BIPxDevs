d239df5287abcee2426de896ba7093632d1335f6
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-09-04 01:13:11 +0900

    update bip100 link

--- BIPApproval/Repository.cs
+++ BIPApproval/Repository.cs
@@ -14,7 +14,7 @@ namespace BIPApproval
 	{
 		public IEnumerable<BIPModel> GetBIPs()
 		{
-			yield return new BIPModel("BIP 100", "https://gtf.org/garzik/bitcoin/BIP100-blocksizechangeproposal.pdf");
+            yield return new BIPModel("BIP 100", "https://github.com/jgarzik/bip100/blob/master/bip-0100.mediawiki");
 			yield return new BIPModel("BIP 101", "https://github.com/bitcoin/bips/blob/master/bip-0101.mediawiki");
 			yield return new BIPModel("BIP 102", "https://github.com/jgarzik/bips/blob/2015_2mb_blocksize/bip-0102.mediawiki");
 			yield return new BIPModel("BIP SIPA", "https://gist.github.com/sipa/c65665fc360ca7a176a6");
